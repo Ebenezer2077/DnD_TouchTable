219b182f34c7038ab4a0c653c247d401de8fd35c
Ebenezer2077 <tobigcb547@outlook.de>
2025-10-09 14:06:12 +0200

    Made Grid Configurable with buttons

--- DnD_NextGen/Szenen/RoomCreator/RoomCreator.cs
+++ DnD_NextGen/Szenen/RoomCreator/RoomCreator.cs
@@ -5,10 +5,34 @@ using System.Collections.Generic;
 public partial class RoomCreator : PanelContainer
 {
     private GridContainer gridContainer;
-    private int columns = 20;
+    private Button AddColumn;
+    private RichTextLabel ColumnCount;
+    private Button RemoveColumn;
+    private VSlider GridSize;
+    private RichTextLabel GridSizeCount;
+    private Button AddRow;
+    private RichTextLabel RowCount;
+    private Button RemoveRow;
+
+    private int columns = 1;
+    private int rows = 1;
+    private int buttonSize = 50;
     public override void _Ready()
     {
         gridContainer = GetNode<GridContainer>("GridContainer");
+        AddColumn = GetNode<Button>("MarginContainer/VBoxContainer/AddColumn");
+        AddColumn.Pressed += () => AdjustColumns(1);
+        ColumnCount = GetNode<RichTextLabel>("MarginContainer/VBoxContainer/RichTextLabel");
+        RemoveColumn = GetNode<Button>("MarginContainer/VBoxContainer/RemoveColumn");
+        RemoveColumn.Pressed += () => AdjustColumns(-1);
+        GridSize = GetNode<VSlider>("MarginContainer/VBoxContainer3/HBoxContainer/VSlider");
+        GridSizeCount = GetNode<RichTextLabel>("MarginContainer/VBoxContainer3/RichTextLabel");
+        AddRow = GetNode<Button>("MarginContainer/VBoxContainer2/AddRow");
+        AddRow.Pressed += () => AdjustRows(1);
+        RowCount = GetNode<RichTextLabel>("MarginContainer/VBoxContainer2/RichTextLabel");
+        RemoveRow = GetNode<Button>("MarginContainer/VBoxContainer2/RemoveRow");
+        RemoveRow.Pressed += () => AdjustRows(-1);
+
         gridContainer.Columns = columns;
     }
     public override void _Input(InputEvent @event)
@@ -17,46 +41,38 @@ public partial class RoomCreator : PanelContainer
         {
             GetTree().ChangeSceneToFile("res://Szenen/MainMenu/MainMenu.tscn");
         }
-        if (@event.IsActionPressed("IncreaseCells"))
-        {
-            columns++;
-            var size = CalculateSize(columns);
-            var amount = CalculateAmount(size);
-            foreach (var button in gridContainer.GetChildren())
-            {
-                gridContainer.RemoveChild(button);
-            }
-            foreach (var button in GenerateGridButtons(amount, size))
-            {
-                gridContainer.AddChild(button);
-            }
-        }
-        if (@event.IsActionPressed("DecreaseCells"))
-        {
-
-        }
     }
 
-    private int CalculateSize(int columns)
+    private void AdjustRows(int amount)
     {
-        return (int)gridContainer.Size.X / columns;
-    }
-
-    private int CalculateAmount(int size)
-    {
-        return ((int)gridContainer.Size.X / size) * ((int)gridContainer.Size.Y / size);
-    }
-
-    private List<Button> GenerateGridButtons(int amount, int size)
-    {
-        var buttonList = new List<Button>();
-        for (int i = 0; i < amount; i++)
+        rows += amount;
+        foreach (var button in gridContainer.GetChildren())
         {
-            var button = GD.Load<PackedScene>("res://Szenen/GridButton/GridButton.tscn").Instantiate<GridButton>();
-            button.CustomMinimumSize = new Vector2(size, size);
-            button.SetSize(new Vector2(size, size));
-            buttonList.Add(button);
+            gridContainer.RemoveChild(button);
         }
-        return buttonList;
+        for (int i = 0; i < rows * columns; i++)
+        {
+            var button = GenerateGridButton();
+            gridContainer.AddChild(button);
+        }
+    }
+    private void AdjustColumns(int amount)
+    {
+        columns += amount;
+        gridContainer.Columns = columns;
+        foreach (var button in gridContainer.GetChildren())
+        {
+            gridContainer.RemoveChild(button);
+        }
+        for (int i = 0; i < rows * columns; i++)
+        {
+            var button = GenerateGridButton();
+            gridContainer.AddChild(button);
+        }
+    }
+    
+    private Button GenerateGridButton()
+    {
+        return GD.Load<PackedScene>("res://Szenen/GridButton/GridButton.tscn").Instantiate<GridButton>();
     }
 }
