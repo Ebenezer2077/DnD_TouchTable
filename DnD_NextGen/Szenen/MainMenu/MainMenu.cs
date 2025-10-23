2a89318417691d7ea09fc4325b7b1f294406b44b
Ebenezer2077 <tobigcb547@outlook.de>
2025-10-23 16:49:48 +0200

    Made foundation for loading a room

--- DnD_NextGen/Szenen/MainMenu/MainMenu.cs
+++ DnD_NextGen/Szenen/MainMenu/MainMenu.cs
@@ -5,17 +5,29 @@ public partial class MainMenu : PanelContainer
 {
     private Button _playButton;
     private Button _createRoomButton;
+    private Button NewRoom;
+    private Button LoadRoom;
+    private PanelContainer RoomOptions;
     public override void _Ready()
     {
+        RoomOptions = GetNode<PanelContainer>("RoomOptions");
+        NewRoom = GetNode<Button>("RoomOptions/MarginContainer/VBoxContainer/HBoxContainer/NewRoom");
+        NewRoom.Pressed += () => GetTree().ChangeSceneToFile("res://Szenen/RoomCreator/RoomCreator.tscn");
+        LoadRoom = GetNode<Button>("RoomOptions/MarginContainer/VBoxContainer/HBoxContainer/LoadRoom");
+        LoadRoom.Pressed += LoadRoomFunc;
         _playButton = GetNode<Button>("MarginContainer2/VBoxContainer/Play");
         _playButton.Pressed += () =>
         {
 
         };
         _createRoomButton = GetNode<Button>("MarginContainer2/VBoxContainer/CreateNewRoom");
-        _createRoomButton.Pressed += () =>
-        {
-            GetTree().ChangeSceneToFile("res://Szenen/RoomCreator/RoomCreator.tscn");
-        };
+        _createRoomButton.Pressed += () => RoomOptions.Visible = true;
+
     }
+
+    private void LoadRoomFunc()
+    {
+        
+    }
+
 }
