a515a91f6de01ecc9251dea57bc930b54f491c62
Ebenezer2077 <tobigcb547@outlook.de>
2025-11-04 17:06:43 +0100

    added popup menu for playstones

--- DnD_NextGen/Szenen/GridButton/GridButton.cs
+++ DnD_NextGen/Szenen/GridButton/GridButton.cs
@@ -3,4 +3,22 @@ using System;
 
 public partial class GridButton : Button
 {
+
+    private Button _gridButton;
+    public Vector2I _position;
+    public bool Playmode = false;
+
+    public Action<Vector2, Vector2I> onPressed;
+
+    public override void _Ready()
+    {
+        _gridButton = GetNode<Button>(".");
+        _gridButton.Pressed += () =>
+        {
+            if (Playmode) onPressed?.Invoke(this.GlobalPosition, _position);
+        };
+    }
+
+
+    
 }
--- DnD_NextGen/Szenen/RoomPlayer/RoomPlayer.cs
+++ DnD_NextGen/Szenen/RoomPlayer/RoomPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public partial class RoomPlayer : Panel
@@ -5,9 +6,10 @@ public partial class RoomPlayer : Panel
     private TextureRect _map;
     private GridContainer _gridcontainer;
     private LoadRoomMenu _loadRoomMenu;
-
+    private PopupMenu _popupMenu;
     public override void _Ready()
     {
+        _popupMenu = GetNode<PopupMenu>("PopupMenu");
         _map = GetNode<TextureRect>("Map");
         _gridcontainer = GetNode<GridContainer>("GridContainer");
         _loadRoomMenu = GetNode<LoadRoomMenu>("LoadRoomMenu");
@@ -29,15 +31,26 @@ public partial class RoomPlayer : Panel
         var roomData = LoadRoomTemplatesProvider.LoadRoom(name);
         var buttonSize = roomData.Item1.ButtonSize;
         var GridPosition = roomData.Item1.GridPosition;
+        var columns = roomData.Item1.GridSize.X;
+        var rows = roomData.Item1.GridSize.Y;
         _map.Texture = roomData.Item2;
         _gridcontainer.Columns = (int)roomData.Item1.GridSize.X;
         _gridcontainer.Position = GridPosition;
-        for(var i = 0; i < roomData.Item1.GridSize.X * roomData.Item1.GridSize.Y; i++)
+        for (var i = 0; i < columns * rows; i++)
         {
             var button = GD.Load<PackedScene>("res://Szenen/GridButton/GridButton.tscn").Instantiate<GridButton>();
+            button.onPressed += OpenButtonpopup;
             button.CustomMinimumSize = new Vector2(buttonSize, buttonSize);
+            button.Playmode = true;
+            button._position = new Vector2I((int)(i % rows), (int)(i / columns));
             _gridcontainer.AddChild(button);
         }
+    }
+    
+    private void OpenButtonpopup(Vector2 globalPosition, Vector2I gridPosition)
+    {
+        _popupMenu.Visible = true;
+        _popupMenu.Position = new Vector2I((int)globalPosition.X, (int)globalPosition.Y);
     }
 
 }
