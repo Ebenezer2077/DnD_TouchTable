9619cd18c51c2ab2823e88f6a8904ca35bb6662a
Ebenezer2077 <tobigcb547@outlook.de>
2025-11-04 15:42:02 +0100

    Created player scene for the room

--- DnD_NextGen/Szenen/MainMenu/MainMenu.cs
+++ DnD_NextGen/Szenen/MainMenu/MainMenu.cs
@@ -5,13 +5,11 @@ public partial class MainMenu : PanelContainer
 {
     private Button _playButton;
     private Button _createRoomButton;
+    
     public override void _Ready()
     {
         _playButton = GetNode<Button>("MarginContainer2/VBoxContainer/Play");
-        _playButton.Pressed += () =>
-        {
-
-        };
+        _playButton.Pressed += () => GetTree().ChangeSceneToFile("res://Szenen/RoomPlayer/RoomPlayer.tscn");
         _createRoomButton = GetNode<Button>("MarginContainer2/VBoxContainer/CreateNewRoom");
         _createRoomButton.Pressed += () => GetTree().ChangeSceneToFile("res://Szenen/RoomCreator/RoomCreator.tscn");
 
